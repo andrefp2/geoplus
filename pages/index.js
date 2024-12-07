20970de214b5df9466cb60e07b31ebe422bd9115
andrefp2 <106488851+andrefp2@users.noreply.github.com>
2024-12-07 01:06:31 +0000

    Pagina em construcção alterada para Jet COlor

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home() {
-  return <h2>Cassia, eu amo você. Se você me ama da uma risadinha. BJS</h2>;
+  return (
+    <h2>
+      Olá querido cliente, a Jet Color esta sendo reinventada. Em breve , você
+      saberá de tudo!
+    </h2>
+  );
 }
 
 export default Home;
